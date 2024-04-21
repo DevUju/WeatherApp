2ac5014ecde06ac9ff0eeaef76361d9eacbcb7dd
Obianuju1998 <agnesanosike@gmail.com>
2024-04-21 13:11:42 +0100

    fully built web app

--- weather.py
+++ weather.py
@@ -5,7 +5,18 @@ import os
 
 load_dotenv()
 
-def get_current_weather(city="Kansas City"):
-    request_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("API key")}&units=imperial"
+def get_current_weather(city):
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("API_KEY")}&units=imperial"
     weather_data = requests.get(request_url).json()
     return weather_data
+
+if __name__ == "__main__":
+    print("\n*** Get Current Weather Condition ***\n")
+    city = input("\nPlease enter a city: ")
+
+    if not bool(city.strip()):
+        city = "Lagos"
+
+    weather_data = get_current_weather(city)
+    print("\n")
+    print(weather_data)
